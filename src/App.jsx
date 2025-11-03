91fdab9e327c79a7d6cf390b5ba7befd918315d2
Developer <developer@company.com>
2025-11-03 10:20:06 +0700

    Auto commit: 2025. 11. 3. 오전 10:20:06

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import About from './pages/About'
 import Services from './pages/Services'
 import Portfolio from './pages/Portfolio'
 import Contact from './pages/Contact'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
                 <Route path="/services" element={<Services />} />
                 <Route path="/portfolio" element={<Portfolio />} />
                 <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<NotFound />} />
               </Routes>
             </main>
             <Footer />
