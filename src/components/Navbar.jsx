5b8a6274d6e64a58abeafb1cfcb1edc52cdb76cb
Developer <developer@company.com>
2025-11-03 09:59:49 +0700

    Auto commit: 2025. 11. 3. 오전 9:59:49

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -78,12 +78,7 @@ const Navbar = () => {
                 >
                   <span className="text-lg">{currentLanguage.flag}</span>
                   <span className="hidden lg:inline">{currentLanguage.name}</span>
-                  <svg
-                    className={`w-4 h-4 transition-transform ${isLanguageOpen ? "rotate-180" : ""}`}
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                  >
+                  <svg className={`w-4 h-4 transition-transform ${isLanguageOpen ? "rotate-180" : ""}`} fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
                   </svg>
                 </button>
@@ -105,7 +100,11 @@ const Navbar = () => {
                         <span>{lang.name}</span>
                         {language === lang.code && (
                           <svg className="w-4 h-4 ml-auto text-navy-900" fill="currentColor" viewBox="0 0 20 20">
-                            <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                            <path
+                              fillRule="evenodd"
+                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                              clipRule="evenodd"
+                            />
                           </svg>
                         )}
                       </button>
@@ -144,7 +143,11 @@ const Navbar = () => {
                         <span>{lang.name}</span>
                         {language === lang.code && (
                           <svg className="w-4 h-4 ml-auto text-navy-900" fill="currentColor" viewBox="0 0 20 20">
-                            <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                            <path
+                              fillRule="evenodd"
+                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                              clipRule="evenodd"
+                            />
                           </svg>
                         )}
                       </button>
